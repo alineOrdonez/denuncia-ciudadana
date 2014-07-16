52710128ed3abc889828c099578d338795639da7
alareog <alareog@gmail.com>
2014-07-16 23:44:40 +0000

    1. Se cambió el package donde se encontraban los Activities 2. Estoy agregando fragmentos a los activities y cambiando los botones al action bar 3. Se han agregado 3 menús 4. Los nombres de los Activities se cambiaron en el string.xml 5. Se agregó el activity SelectLocationActivity con el cual seleccionas la ubicación actual de la denuncia 6. Se actualizó el archivo AndroidManifest.xml

---
+++  denuncia-ciudadana/src/com/upiicsa/denuncia/controller/MainMenuActivity.java
@@ -0,0 +1,53 @@
+package com.upiicsa.denuncia.controller;
+
+import com.upiicsa.denuncia.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainMenuActivity extends Activity {
+
+	Button denunciaButton, consultaButton;
+	Context context;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+		context = this;
+		addListenerOnButton();
+	}
+
+	public void addListenerOnButton() {
+
+		denunciaButton = (Button) findViewById(R.id.denuncia);
+		consultaButton = (Button) findViewById(R.id.consulta);
+
+		denunciaButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent i = new Intent(context, ComplaintActivity.class);
+				startActivity(i);
+			}
+		});
+
+		consultaButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent i = new Intent(context, ConsultActivity.class);
+				startActivity(i);
+			}
+		});
+	}
+
+	@Override
+	public void onBackPressed() {
+	}
+}
